5f832e862c87ed119bdd9c5d967770ac3317b8ff
Rishabh Pathak <clusmsyknight@gmail.com>
2023-05-24 20:31:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div id="App">
+      <div className="w-screen h-screen flex flex-row items-center justify-center text-lg">HOME</div>
+    </div>
+  );
+}
+
+export default App;
