fd4551bf22060a455bd6a166c8adc896eb1f08f8
Rishabh Pathak <clusmsyknight@gmail.com>
2023-07-11 00:00:13 +0530

    deploy

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,7 @@
 function About() {
-  return <div className="w-screen h-screen flex flex-col items-center justify-center text-4xl overflow-hidden">About</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center text-4xl overflow-hidden">About</div>
+  );
 }
 
 export default About;
